2e2e949c4546347680ba2370e78f4d39e08c2a0e
Otavio Fernandes <otaviof@gmail.com>
2023-02-23 04:07:02 +0100

    Testing
    Asserting the action is performing as expected.

---
+++ assert.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Assert the changes made by this action.
+#
+
+shopt -s inherit_errexit
+set -eu -o pipefail
+
+source common.sh
+
+phase "Asserting the Container Registry rollout status"
+rollout_status "${REGISTRY_NAMESPACE}" "registry"
+
+phase "Asserting the /etc/hosts have been patched"
+if ! grep -E -q '127.0.0.1.*registry' /etc/hosts ; then
+	fail "/etc/hosts does not include the registry hostname"
+fi
+
+phase "Asserting the Tekton Pipeline Controller"
+rollout_status "${TEKTON_NAMESPACE}" "tekton-pipelines-controller"
+
+phase "Asserting the Tekton Pipeline WebHook"
+rollout_status "${TEKTON_NAMESPACE}" "tekton-pipelines-webhook"
+
+phase "Asserting the CLI (tkn) is installed"
+probe_bin_on_path "tkn"
